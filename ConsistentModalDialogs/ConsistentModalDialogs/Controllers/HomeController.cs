67ea6ae4a3a29fb99724efe306e0ffaac9219462
Darrell Mozingo <darrell@mozingo.net>
2011-07-20 09:10:11 -0400

    Added an empty projcct for the modal post

---
+++ ConsistentModalDialogs/ConsistentModalDialogs/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace ConsistentModalDialogs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ConsistentModalDialogs/ConsistentModalDialogs/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ConsistentModalDialogs.MvcApplication" Language="C#" %>
